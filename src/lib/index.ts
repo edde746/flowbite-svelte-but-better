227bc4ab27900584b34900342f15bb949c3cec1e
edde746 <86283021+edde746@users.noreply.github.com>
2022-05-13 01:04:06 +0200

    Breadcrumb

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,8 @@ export { default as Alert } from './Alert/Alert.svelte';
 export { default as Avatar } from './Avatar/Avatar.svelte';
 export { default as AvatarGroup } from './Avatar/AvatarGroup.svelte';
 export { default as AvatarGroupCounter } from './Avatar/AvatarGroupCounter.svelte';
+/* Breadcrumb */
+export { default as BreadcrumbItem } from './Breadcrumb/BreadcrumbItem.svelte';
+export { default as Breadcrumb } from './Breadcrumb/Breadcrumb.svelte';
 /* Badge */
 export { default as Badge } from './Badge/Badge.svelte';
