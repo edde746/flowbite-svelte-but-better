fab23d1f285fd7ac07178338a89914b134bbd71a
edde746 <86283021+edde746@users.noreply.github.com>
2022-05-13 00:56:54 +0200

    Badge

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+/* Alert */
+export { default as Alert } from './Alert/Alert.svelte';
+/* Avatar */
+export { default as Avatar } from './Avatar/Avatar.svelte';
+export { default as AvatarGroup } from './Avatar/AvatarGroup.svelte';
+export { default as AvatarGroupCounter } from './Avatar/AvatarGroupCounter.svelte';
+/* Badge */
+export { default as Badge } from './Badge/Badge.svelte';
