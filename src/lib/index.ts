9f01f0d8257ddd0c5eb7b9189f9b782353c58321
edde746 <86283021+edde746@users.noreply.github.com>
2022-05-13 12:21:30 +0200

    Footer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,13 @@ export { default as DropdownDivider } from './Dropdown/DropdownDivider.svelte';
 export { default as DropdownHeader } from './Dropdown/DropdownHeader.svelte';
 export { default as Dropdown } from './Dropdown/Dropdown.svelte';
 export { default as DropdownItem } from './Dropdown/DropdownItem.svelte';
+/* Footer */
+export { default as FooterCopyright } from './Footer/FooterCopyright.svelte';
+export { default as FooterLink } from './Footer/FooterLink.svelte';
+export { default as FooterIcon } from './Footer/FooterIcon.svelte';
+export { default as Footer } from './Footer/Footer.svelte';
+export { default as FooterLinkGroup } from './Footer/FooterLinkGroup.svelte';
+export { default as FooterBrand } from './Footer/FooterBrand.svelte';
 /* Tooltip */
 export { default as Tooltip } from './Tooltip/Tooltip.svelte';
 /* Card */
